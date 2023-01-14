0a78f766f257ef699629ef260656fa7cbc2f3e02
Kaffff <takahashi.prog@gmail.com>
2023-01-14 22:30:31 +0900

    getUsers

--- src/script.ts
+++ src/script.ts
@@ -4,13 +4,8 @@ const prisma = new PrismaClient();
 
 async function main() {
   // ... you will write your Prisma Client queries here
-  const user = await prisma.user.create({
-    data: {
-      name: "Alice",
-      email: "alice@prisma.io",
-    },
-  });
-  console.log(user);
+  const users = await prisma.user.findMany();
+  console.log(users);
 }
 
 main()
